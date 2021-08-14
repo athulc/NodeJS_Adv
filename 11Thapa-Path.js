50b6c93c029e647eee9fa4795d978f088ec73610
Athul C <athul@jivrus.com>
2021-08-14 22:32:04 +0530

    Path module in NodeJS

---
+++ 11Thapa-Path.js
@@ -0,0 +1,6 @@
+const path = require('path');
+
+console.log(path.dirname('F:/NodeJS_Adv/Projects/11Thapa-Path.js'));
+console.log(path.basename('F:/NodeJS_Adv/Projects/11Thapa-Path.js'));
+console.log(path.extname('F:/NodeJS_Adv/Projects/11Thapa-Path.js'));
+console.log(path.parse('F:/NodeJS_Adv/Projects/11Thapa-Path.js'));
